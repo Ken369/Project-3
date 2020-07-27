97367a274ea1613a0ccfc3da7ac38df5a9b8cb55
Ken Power <kpowerperth@gmail.com>
2020-07-27 19:02:43 +0800

    Inst Mern Cut Paste

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Garagedoor</h2>
+      </div>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+
+export default App;
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import {Typography} from "@material-ui/core";
+// eslint-disable-next-line
+import Card from '@material-ui/core/Card';
+// eslint-disable-next-line
+import CardActions from '@material-ui/core/CardActions';
+// eslint-disable-next-line
+import CardContent from '@material-ui/core/CardContent';
+// eslint-disable-next-line
+import Button from '@material-ui/core/Button';
+// eslint-disable-next-line
+import Menu from '@material-ui/core/Menu';
+// eslint-disable-next-line
+import MenuItem from '@material-ui/core/MenuItem';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12
+          <Typography variant="h1" color="primary">
+            Hey Mate
+          </Typography>
+          <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        Open Menu
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
